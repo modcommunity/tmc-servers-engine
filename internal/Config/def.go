2ce52317f75b346aa58b849eb3a8efb1114fb14a
Christian Deacon <christian@moddingcommunity.com>
2022-10-24 02:15:12 +0000

    Prepare config for new changes for engines, etc.

--- internal/Config/def.go
+++ internal/Config/def.go
@@ -1,12 +1,15 @@
 package Config
 
 type Config struct {
-	Debug       uint   `json:"debug"`
-	RetrieveURL string `json:"retrieveurl"`
-	UpdateURL   string `json:"updateurl"`
-	PostHook    string `json:"posthook"`
-	BasicAuth   bool   `json:"basicauth"`
-	Token       string `json:"token"`
+	Debug uint `json:"debug"`
+
+	RetrieveEnginesURL string `json:"reurl"`
+	RetrieveURL        string `json:"retrieveurl"`
+	UpdateURL          string `json:"updateurl"`
+	PostHook           string `json:"posthook"`
+
+	BasicAuth bool   `json:"basicauth"`
+	Token     string `json:"token"`
 
 	// For basic auth. Name of parameter we're using with the token.
 	KeyParam string `json:"keyparam"`
@@ -30,6 +33,7 @@ func (cfg *Config) SetDefaults() {
 	cfg.ClaimKeyField = "claimkey"
 
 	// Both same value for now, but still want them to be different config options in case.
+	cfg.RetrieveEnginesURL = "https://mydomain.example/servers/engines"
 	cfg.RetrieveURL = "https://mydomain.example/servers/servers"
 	cfg.UpdateURL = "https://mydomain.example/servers/servers"
 	cfg.PostHook = "https://mydomain.example/servers/stats"
